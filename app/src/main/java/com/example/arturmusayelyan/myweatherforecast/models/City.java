16f562a91561d5fddadf6c2cff73c0bf066b2a48
artur.musayelyan <musayelyan099@gmail.com>
2018-02-19 10:31:54 +0400

    19.02.2018 ArtCommit gorcic ( 1 )

---
+++ app/src/main/java/com/example/arturmusayelyan/myweatherforecast/models/City.java
@@ -0,0 +1,70 @@
+package com.example.arturmusayelyan.myweatherforecast.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by artur.musayelyan on 14/02/2018.
+ */
+
+public class City {
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("country")
+    @Expose
+    private String country;
+    @SerializedName("coord")
+    @Expose
+    private Coord coord;
+
+    public City() {
+    }
+
+    public City(Integer id, String name, String country, Coord coord) {
+        this.id = id;
+        this.name = name;
+        this.country = country;
+        this.coord = coord;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Coord getCoord() {
+        return coord;
+    }
+
+    public void setCoord(Coord coord) {
+        this.coord = coord;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" + "id=" + id + ", name='" + name + '\'' + ", country='" + country + '\'' + ", coord=" + coord + '}';
+    }
+}
