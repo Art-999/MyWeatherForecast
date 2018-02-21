18f5a0298835d18d1f7172c4c0e31a1ab112e1c5
artur.musayelyan <musayelyan099@gmail.com>
2018-02-21 19:01:55 +0400

    21.02.2018 ArtCommit gorcic ( 3 )

--- app/src/main/java/com/example/arturmusayelyan/myweatherforecast/adapters/RecyclerCityAdapter.java
+++ app/src/main/java/com/example/arturmusayelyan/myweatherforecast/adapters/RecyclerCityAdapter.java
@@ -1,8 +1,6 @@
 package com.example.arturmusayelyan.myweatherforecast.adapters;
 
 import android.content.Context;
-import android.graphics.drawable.Drawable;
-import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,14 +8,10 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.DataSource;
-import com.bumptech.glide.load.engine.GlideException;
-import com.bumptech.glide.request.RequestListener;
-import com.bumptech.glide.request.target.Target;
 import com.example.arturmusayelyan.myweatherforecast.R;
 import com.example.arturmusayelyan.myweatherforecast.RecyclerCityClick;
 import com.example.arturmusayelyan.myweatherforecast.models.WeatherList;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -60,19 +54,24 @@ public class RecyclerCityAdapter extends RecyclerView.Adapter<RecyclerCityAdapte
         holder.cityTv.setText(currentWeatherName);
         holder.tempratureTv.setText((int)Double.parseDouble(String.valueOf(currentWeather.getMain().getTemp()))+"º C");
 
-        Glide.with(context).load("http://openweathermap.org/img/w/"+dataList.get(holder.getAdapterPosition()).getWeather().get(0).getIcon()+".png").listener(new RequestListener<Drawable>() {
-            @Override
-            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
-                return false;
-            }
+        holder.weatherIcon.setImageDrawable(null);
 
-            @Override
-            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
-//holder.weatherIcon.setImageDrawable(resource);
-                holder.weatherIcon.setImageDrawable(null);
-                return false;
-            }
-        }).into(holder.weatherIcon);
+       // Glide.with(context).load("http://openweathermap.org/img/w/"+dataList.get(holder.getAdapterPosition()).getWeather().get(0).getIcon()+".png").into(holder.weatherIcon);
+        holder.weatherIcon.setImageDrawable(null);
+        Picasso.with(context).load("http://openweathermap.org/img/w/"+dataList.get(holder.getAdapterPosition()).getWeather().get(0).getIcon()+".png").into(holder.weatherIcon);
+//                listener(new RequestListener<Drawable>() {
+//            @Override
+//            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+////holder.weatherIcon.setImageDrawable(resource);
+//                holder.weatherIcon.setImageDrawable(null);
+//                return false;
+//            }
+//        }).into(holder.weatherIcon);
 
     }
 
